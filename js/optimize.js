d08c51ace3bf91d4e6846cd0e08f16ae4b74067a
Joshua Gollaher <joshg@mopine.com>
2025-09-07 13:31:00 -0500

    Implemented constant-folding.

--- js/optimize.js
+++ js/optimize.js
@@ -6,4 +6,5 @@ let y = x + Math.random();
 let z = 2 + 2;
 let zz = 4 + x;
 
+// Tree shaking
 function if_you_see_this_then_dce_does_not_work() {}
--- src/optim/optim.rs
+++ src/optim/optim.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
-use crate::parser::{Expression, Literal, Statement, AST};
+use log::trace;
+use crate::parser::{BinaryOperator, Expression, Literal, Statement, UnaryOperator, AST};
 
 #[derive(Clone)]
+#[derive(Debug)]
 enum ConstVal {
     StringLiteral(String),
     Number(f64),
@@ -69,6 +71,7 @@ impl Optimizer {
             Expression::Literal(l) => Expression::Literal(l),
             Expression::Identifier(id) => {
                 if let Some(saved_const) = self.get_constant(id.as_str()) {
+                    trace!("Propagating constant: {id} = {saved_const:?}");
                     saved_const.into_expression()
                 } else {
                     Expression::Identifier(id)
@@ -165,18 +168,114 @@ impl Optimizer {
         }
     }
 
-    fn constant_folding(&mut self) {
-
+    fn fold_statement(&mut self, stmt: Statement) -> Statement {
+        match stmt {
+            Statement::Expression(expr) => Statement::Expression(self.fold_expression(*expr).into()),
+            Statement::Return(expr) => Statement::Return(self.fold_expression(*expr).into()),
+            Statement::If { condition, consequence, alternative } => Statement::If { condition: self.fold_expression(*condition).into(), consequence: self.fold_statement(*consequence).into(), alternative: alternative.map(|alt| self.fold_statement(*alt.clone()).into()) },
+            Statement::While { condition, body } => Statement::While { condition: self.fold_expression(*condition).into(), body: self.fold_statement(*body).into() },
+            Statement::For { init, condition, update, body } => {
+                Statement::For {
+                    init: init.map(|init| self.fold_statement(*init.clone()).into()),
+                    condition: condition.map(|condition| self.fold_expression(*condition.clone()).into()),
+                    update: update.map(|update| self.fold_expression(*update.clone()).into()),
+                    body: self.fold_statement(*body).into(),
+                }
+            }
+            Statement::Function { name, args, body } => Statement::Function { name, args, body: self.fold_statement(*body).into() },
+            Statement::Scope { statements } => {
+                let statements = statements.into_iter().map(|stmt| self.fold_statement(stmt)).collect();
+                Statement::Scope { statements }.into()
+            }
+            Statement::Let { name, value } => Statement::Let { name, value: self.fold_expression(*value).into() },
+        }
     }
 
-    fn tree_shaking(&mut self) {
+    fn fold_expression(&mut self, expr: Expression) -> Expression {
+        match expr {
+            e @  Expression::Literal(..) => e,
+            e @ Expression::Identifier(..) => e,
+            Expression::Object { properties } => {
+                Expression::Object {
+                    properties: properties.into_iter().map(|(k, v)| (k, self.fold_expression(*v).into())).collect(),
+                }
+            },
+            Expression::Array { elements } => Expression::Array { elements: elements.into_iter().map(|el| self.fold_expression(*el).into()).collect() },
+            Expression::BinaryOp { left, op, right } => {
+                match (*left.clone(), op.clone(), *right.clone()) {
+                    (Expression::Literal(Literal::Number(l)), BinaryOperator::Add, Expression::Literal(Literal::Number(r))) => {
+                        trace!("Folding {l} + {r} into {}", l + r);
+                        Expression::Literal(Literal::Number(l + r))
+                    },
+                    (Expression::Literal(Literal::String(l)), BinaryOperator::Add, Expression::Literal(Literal::String(r))) => {
+                        trace!("Folding '{l}' + '{r}' into '{}'", l.clone() + r.as_str());
+                        Expression::Literal(Literal::String(l.clone() + r.as_str()))
+                    },
+                    (Expression::Literal(Literal::Number(l)), BinaryOperator::Sub, Expression::Literal(Literal::Number(r))) => {
+                        trace!("Folding {l} - {r} into {}", l + r);
+                        Expression::Literal(Literal::Number(l - r))
+                    },
+                    (Expression::Literal(Literal::Number(l)), BinaryOperator::Mul, Expression::Literal(Literal::Number(r))) => {
+                        trace!("Folding {l} * {r} into {}", l * r);
+                        Expression::Literal(Literal::Number(l * r))
+                    },
+                    (Expression::Literal(Literal::Number(l)), BinaryOperator::Div, Expression::Literal(Literal::Number(r))) => {
+                        trace!("Folding {l} / {r} into {}", l / r);
+                        Expression::Literal(Literal::Number(l / r))
+                    },
+                    (Expression::Literal(Literal::Number(l)), BinaryOperator::Mod, Expression::Literal(Literal::Number(r))) => {
+                        trace!("Folding {l} % {r} into {}", l % r);
+                        Expression::Literal(Literal::Number(l % r))
+                    },
+                    _ => Expression::BinaryOp { left: self.fold_expression(*left.clone()).into(), op: op.clone(), right: self.fold_expression(*right.clone()).into() },
+                }
+            },
+            Expression::UnaryOp { op, expr } => {
+                match (op.clone(), *expr.clone()) {
+                    (UnaryOperator::Negate, Expression::Literal(Literal::Number(n))) => {
+                        trace!("Folding -{n} into {}", -n);
+                        Expression::Literal(Literal::Number(-n))
+                    },
+                    (UnaryOperator::Not, Expression::Literal(Literal::Boolean(b))) => {
+                        trace!("Folding !{b} into {}", !b);
+                        Expression::Literal(Literal::Boolean(!b))
+                    },
+                    _ => Expression::UnaryOp { op: op.clone(), expr: self.fold_expression(*expr.clone()).into()}
+                }
+            },
+            Expression::FunctionCall { callee, args } => {
+                Expression::FunctionCall { callee: self.fold_expression(*callee).into(), args: args.into_iter().map(|arg| self.fold_expression(*arg).into()).collect() }
+            },
+            Expression::Assignment { target, value } => Expression::Assignment { target, value: self.fold_expression(*value).into() },
+            Expression::Index { target, index } => Expression::Index { target, index: self.fold_expression(*index).into() },
+            e @ Expression::Property { .. } => e,
+        }
+    }
 
+    fn constant_folding(&mut self) {
+        let mut stmts = self.ast.statements.clone();
+        stmts = stmts.into_iter().map(|stmt| self.fold_statement(stmt)).collect();
+
+        self.ast.statements = stmts;
+    }
+
+    fn unroll_statement(&mut self, stmt: Statement) -> Statement {
+        stmt
     }
 
     fn loop_unrolling(&mut self) {
         // Conditions
         // - Loop bounds and increment are known AOT
         // - No variables inside body, we're not doing substitution.
+
+        let mut stmts = self.ast.statements.clone();
+        stmts = stmts.into_iter().map(|stmt| self.unroll_statement(stmt)).collect();
+
+        self.ast.statements = stmts;
+    }
+
+    fn tree_shaking(&mut self) {
+
     }
 
     pub fn optimize(&mut self) -> AST {
